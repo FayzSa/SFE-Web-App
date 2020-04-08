29630f0ec9e7704f0866edefaecde78f830003a0
fayzsa <fayz.sabir@gmail.com>
2020-04-08 23:32:05 +0100

    Make a Structer Of App

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\MailModel;
+use Illuminate\Http\Request;
+
+class MailController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\MailModel  $mailModel
+     * @return \Illuminate\Http\Response
+     */
+    public function show(MailModel $mailModel)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\MailModel  $mailModel
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(MailModel $mailModel)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\MailModel  $mailModel
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, MailModel $mailModel)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\MailModel  $mailModel
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(MailModel $mailModel)
+    {
+        //
+    }
+}
